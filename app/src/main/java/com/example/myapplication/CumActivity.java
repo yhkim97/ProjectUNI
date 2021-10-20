8c94dc038840972eec39f6ad8b1a282862dcbbf4
yhkim97 <rladydgml82@naver.com>
2021-10-20 22:12:19 +0900

    디자인 완료(게시판X) / 화면구성중(activity_place 완료)

---
+++ app/src/main/java/com/example/myapplication/CumActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class CumActivity extends MainActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cummu);
+
+        // 로그인 버튼 클릭시 액티비티 전환
+        Button imageButton = (Button) findViewById(R.id.login);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // 문화재 검색 버튼클릭시 액티비티 전환
+        Button search_cult = (Button) findViewById(R.id.search_cult);
+        search_cult.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), PlaceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //위치 검색 버튼 클릭시 액티비티 전환
+        Button search_site = (Button) findViewById(R.id.search_site);
+        search_site.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), ZoneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //커뮤니티 버튼 클릭시 액티비티 전환
+        Button search_cum = (Button) findViewById(R.id.search_cum);
+        search_cum.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), CumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
