7831e032ea74be8da339368fab92a2a204e4698d
home <audrbchzl@gmali.com>
2021-11-03 20:37:04 +0900

    지도 페이지 구현

--- app/src/main/java/com/example/myapplication/ZoneActivity.java
+++ app/src/main/java/com/example/myapplication/ZoneActivity.java
@@ -1,27 +1,93 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import androidx.annotation.NonNull;
 
-public class ZoneActivity extends MainActivity{
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.widget.Toast;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.overlay.PathOverlay;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.Arrays;
+
+public class ZoneActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private FusedLocationSource mLocationSource;
+    private NaverMap MnaverMap;
+    LatLng prev_LOC = null;
+    LatLng curr_LOC;
+
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zone1);
+        setContentView(R.layout.activity_zone);
 
-        // 카메라 인식 버튼 클릭시 액티비티 전환
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
+        // 지도를 출력할 르레그먼트 영역 인식
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
 
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), CamActivity.class);
-                startActivity(intent);
-            }
-        });
+        // 지도 사용이 준비되면 onMapReady 콜백 함수 호출
+        mapFragment.getMapAsync(this);
+
+        // 위치를 반환하는 구현체인 FusedLocationSource 생성
+        mLocationSource =
+                new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+    }
+
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.MnaverMap = naverMap;
+        MnaverMap.setLocationSource(mLocationSource);
+
+        ActivityCompat.requestPermissions(this,PERMISSIONS,PERMISSION_REQUEST_CODE);
 
     }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // request code와 권한획득 여부 확인
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                MnaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
+    }
 }
+
+
+
+
