2c61c5043f56ed4b9c31dacf66c8a6fa5bd6ba62
home <audrbchzl@gmali.com>
2021-11-03 23:19:16 +0900

    지도 페이지 구현

--- app/src/main/java/com/example/myapplication/ZoneActivity.java
+++ app/src/main/java/com/example/myapplication/ZoneActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.icu.text.IDNA;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,6 +16,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.view.View;
 import android.widget.Toast;
 
 import com.naver.maps.geometry.LatLng;
@@ -22,24 +24,27 @@ import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.PathOverlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import java.util.Arrays;
 
 public class ZoneActivity extends FragmentActivity implements OnMapReadyCallback {
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    private static final String[] PERMISSIONS = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-    };
 
     private FusedLocationSource mLocationSource;
     private NaverMap MnaverMap;
     LatLng prev_LOC = null;
     LatLng curr_LOC;
-
+    LatLng Loc1 = new LatLng(37.47568,126.61738);
+    LatLng Loc2 = new LatLng(37.43940,126.68724);
+    LatLng Loc3 = new LatLng(37.71341,126.45151);
+    Marker marker1 = new Marker();
+    Marker marker2 = new Marker();
+    Marker marker3 = new Marker();
+    InfoWindow infoWindow = new InfoWindow();
 
     LocationManager locationManager;
     LocationListener locationListener;
@@ -59,10 +64,6 @@ public class ZoneActivity extends FragmentActivity implements OnMapReadyCallback
 
         // 지도 사용이 준비되면 onMapReady 콜백 함수 호출
         mapFragment.getMapAsync(this);
-
-        // 위치를 반환하는 구현체인 FusedLocationSource 생성
-        mLocationSource =
-                new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
     }
 
 
@@ -70,21 +71,36 @@ public class ZoneActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         this.MnaverMap = naverMap;
-        MnaverMap.setLocationSource(mLocationSource);
 
-        ActivityCompat.requestPermissions(this,PERMISSIONS,PERMISSION_REQUEST_CODE);
-
-    }
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // request code와 권한획득 여부 확인
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                MnaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-            }
+        marker1.setTag("인천 차이나타운");
+        marker1.setPosition(Loc1);
+        marker1.setMap(naverMap);
+        marker1.setOnClickListener(overlay -> {
+            infoWindow.open(marker1);
+            return true;
+        });
+        marker2.setTag("인천 향교");
+        marker2.setPosition(Loc2);
+        marker2.setMap(naverMap);
+        marker2.setOnClickListener(overlay -> {
+            infoWindow.open(marker2);
+            return true;
+        });
+        marker3.setTag("인천 강화도");
+        marker3.setPosition(Loc3);
+        marker3.setMap(naverMap);
+        marker3.setOnClickListener(overlay -> {
+            infoWindow.open(marker3);
+            return true;
+        });
+    infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+        @NonNull
+        @Override
+        public CharSequence getText(@NonNull InfoWindow infoWindow) {
+            return (CharSequence)infoWindow.getMarker().getTag();
         }
+    });
+
     }
 }
 
